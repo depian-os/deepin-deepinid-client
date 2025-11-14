19ffedd32bf918d678a90efac5e5e60472c928c9
lhdjply <lhdjply@126.com>
2025-11-14 10:10:34 +0800

    上传源码
    Signed-off-by: lhdjply <lhdjply@126.com>

---
+++ translations/deepin-deepinid-client_en_US.ts
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_AS">
+<context>
+    <name>QObject</name>
+    <message>
+        <source>deepin ID</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Synchronization of deepin ID login status failed</source>
+        <translation>Synchronization of deepin ID login status failed</translation>
+    </message>
+    <message>
+        <source>Due to network anomalies, your deepin ID login status could not be synchronized. Please log in manually or try again later</source>
+        <translation>Due to network anomalies, your deepin ID login status could not be synchronized. Please log in manually or try again later</translation>
+    </message>
+    <message>
+        <source>View</source>
+        <translation>View</translation>
+    </message>
+</context>
+</TS>
